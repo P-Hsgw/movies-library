988b6f665615a90662580fe808c2ebdcb4816640
P-Hsgw <contact@p-hsgw.com>
2021-11-29 23:32:45 +0900

    Create basic structure of the app

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"; 
+
+export const Header = () => {
+
+  return (
+    <div className="flex flex-wrap justify-center content-center w-screen h-12 ">
+      Test dsasa  
+    </div>
+  )
+}
