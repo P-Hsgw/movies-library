06e6ec9c049659776984034d66613b01ff898cd2
P-Hsgw <contact@p-hsgw.com>
2021-11-30 18:18:17 +0900

    Update card sizes

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ import { Card } from "./Card"
 export const Body = () => {
 
   return ( 
-    <div className="w-screen grid xl:grid-cols-5 gap-4 lg:grid-cols-3 md:grid-cols-2 mt-20">
+    <div className="w-screen grid xl:grid-cols-5 gap-8 lg:grid-cols-3 md:grid-cols-2 mt-20">
     <Card />
     <Card />
     <Card />
@@ -12,7 +12,6 @@ export const Body = () => {
     <Card />
     <Card />
     <Card />
-
     </div>
 
   )
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,15 @@ import { Button  } from "./Button";
 export const Card = () => {
   return(
     <div className=" flex items-center justify-center">
-    <div className="border-4 border-solid rounded-md h-72 w-72 text-center divide-y divide-fuchsia-300">
+    <div className="border-4 border-solid rounded-md w-96 text-center divide-y divide-fuchsia-300 h-min-max">
       <div className="p-4">
         MOVIE TITLE - release date
       </div>
-      <div className="p-14">
+      <div className="p-28">
         MOVIE THUMBNAIL
       </div>
       <div>
-        <Button>Watched </ Button>
+        <Button>Watched</ Button>
         <Button>Want to watch</Button>
       </div>
     </div></div>
