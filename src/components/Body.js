16ec4971a873a1ddd027834a5affa6e82d88b214
P-Hsgw <contact@p-hsgw.com>
2021-11-30 17:57:37 +0900

    Add basic card layout

---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+import { Card } from "./Card"
+
+export const Body = () => {
+
+  return ( 
+    <Card />
+  )
+}
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react"; 
+import { Button  } from "./Button";
+
+export const Card = () => {
+  return(
+    <div className="border-4 border-solid rounded-md h-72 w-72 m-8 text-center divide-y divide-fuchsia-300">
+      <div className="p-4">
+        MOVIE TITLE - release date
+      </div>
+      <div className="p-14">
+        MOVIE THUMBNAIL
+      </div>
+      <div>
+        <Button>Watched </ Button>
+        <Button>Want to watch</Button>
+      </div>
+    </div>
+  )
+}
