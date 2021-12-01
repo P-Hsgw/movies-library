169b82c1d71c973eed6a7474bf5ad7428b1f27f8
P-Hsgw <contact@p-hsgw.com>
2021-12-01 16:47:09 +0900

    Add a logic to fill out cards with props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,19 @@
-import React from "react"; 
-import { Button  } from "./Button";
+import React from "react";
+import { Button } from "./Button";
 
-export const Card = () => {
-  return(
+export const Card = ({ title, releaseDate, thumbnail, isWatched }) => {
+  return (
     <div className=" flex items-center justify-center ">
-    <div className="border-4 border-solid rounded-md w-89 text-center divide-y divide-fuchsia-300 h-min-max">
-      <div className="p-4">
-        MOVIE TITLE - release date
+      <div className="border-4 border-solid rounded-md w-89 text-center divide-y divide-fuchsia-300 h-min-max">
+        <div className="p-4">
+          {title} - {releaseDate}
+        </div>
+        <div className="p-28">{thumbnail}</div>
+        <div>
+          <Button additionalClass="mb-4">Watched</Button>
+          <Button additionalClass="mb-4">Want to watch</Button>
+        </div>
       </div>
-      <div className="p-28">
-        MOVIE THUMBNAIL
-      </div>
-      <div>
-        <Button additionalClass="mb-4">Watched</ Button>
-        <Button additionalClass="mb-4">Want to watch</Button>
-      </div>
-    </div></div>
-  )
-}
+    </div>
+  );
+};
